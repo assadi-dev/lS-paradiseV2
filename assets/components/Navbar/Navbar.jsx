71dfc514d8d129d10663d8562c089277732093f7
Assadi <assadi.halifa@gmail.com>
2021-06-26 12:49:44 +0200

    correction dropdown nav

--- assets/components/Navbar/Navbar.jsx
+++ assets/components/Navbar/Navbar.jsx
@@ -9,15 +9,31 @@ import { HashLink} from 'react-router-hash-link';
 export class Navbar extends Component {
   state = {
     navbarScroll: false,
+    dropdownMenu:false
   };
 
   componentDidMount() {
     window.addEventListener("scroll", this.handleScroll);
+
+    document.querySelectorAll(".dropdown a").forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        e.preventDefault();
+       
+        this.setState({...this.state,dropdownMenu:!this.state.dropdownMenu})
+      })
+    })
+
   }
 
   componentWillUnmount() {
     window.removeEventListener("scroll", this.handleScroll);
   }
+
+  showMenu = (e) => {
+    e.peventDefault();
+    this.setState({...this.state,dropdownMenu:!this.state.dropdownMenu})
+   }
+
 
   handleScroll = () => {
     if (window.scrollY > 20) {
@@ -49,14 +65,20 @@ export class Navbar extends Component {
           <div className="navCont">
           <ul className="navbar-nav-left">
               <li className="nav-item">
-                <HashLink smooth to="/#top"  >Accueil </HashLink>
+                <HashLink className="azonix-font" smooth to="/#top"  >Accueil </HashLink>
               </li>
               <li className="nav-item">
                
-                <HashLink smooth to="/#join-section"  > Nous rejoindre </HashLink>
+                <HashLink className="azonix-font" smooth to="/#join-section"  > Nous rejoindre </HashLink>
               </li>
-            <li className="nav-item">Discord</li>
-            <li className="nav-item">Vote</li>
+            <li className="nav-item"> <a className="azonix-font" href=""> Discord</a></li>
+              <li className="nav-item dropdown" ><a className="azonix-font" href="/vote"  >Vote</a>
+                <div className={classNames("dropdown-menu",this.state.dropdownMenu?"show":null)} >
+                  <a href="">Top Server</a>
+                  <div className="dropdown-divider"></div>
+                  <a href="">Serveur privée</a>
+                </div>
+              </li>
           </ul>
           <div className="navbar-right"> 
             <span id="login-btn"> <i id="icone-login"><FontAwesomeIcon  icon={faUser}  /></i> Se connecter</span>
